bf82712697ee43582a64c71d597f8a608559af15
法外狂徒张三 <46189169+kelecn@users.noreply.github.com>
2021-01-03 19:48:06 +0800

    Upload by PicGo

---
+++ weather.js
@@ -0,0 +1,1 @@
+document.writeln("<iframe scrolling=\'no\' src=\'https://tianqiapi.com/api.php?style=tv&skin=sogou\' frameborder=\'0\' width=\'120\' height=\'20\' allowtransparency=\'true\'></iframe>");
